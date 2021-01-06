f22398ccf3ba9e8fa430133c948233172cd6f824
paul-schultz <paul.william.schultz@gmail.com>
2021-01-05 22:31:49 -0800

    react app initialized, concurrent dev server, proxy

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+      <a href="http://localhost:5000/api/df_text_query">Some stuff from backend</a>
+    </div>
+  );
+}
+
+export default App;
