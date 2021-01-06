97a1efb5615d522029491ad7639e89296cb8b3d9
paul-schultz <paul.william.schultz@gmail.com>
2021-01-05 18:05:58 -0800

    added env variables

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-    googleProjectID: 'brewski-huxe',
-    dialogFlowSessionID: 'brewski-bot-session',
-    dialogFlowSessionLanguageCode: 'en-US'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev')
 }
-
