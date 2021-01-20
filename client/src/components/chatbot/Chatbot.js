0f2aecfd095d09c4e40d95f36718588dae812653
paul-schultz <paul.william.schultz@gmail.com>
2021-01-19 21:34:43 -0800

    clean up Chatbot.js code

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -45,125 +45,7 @@ class Chatbot extends Component {
                 }
             } 
         };
-
         this.setState({messages: [...this.state.messages, says]});
-        // try {
-
-        //     let action = res.data.action;
-        //     let anyEntity = res.data.parameters.fields.any;
-        //     let cityEntity = res.data.parameters.fields["geo-city"];
-
-        //     for (let msg of res.data.fulfillmentMessages) {
-        //         // Change JSON response structure dependent on action
-        //             if (action === 'get-name' || action === 'get-city') {
-        //             console.log(`Action: ${action}`)
-        //             let ent = ''
-        //             let searchBy = ''
-        //             if (anyEntity !== undefined) {
-        //                 ent = anyEntity.stringValue
-        //                 searchBy = 'name'
-        //             } else if (cityEntity !== undefined) {
-        //                 ent = cityEntity.stringValue
-        //                 searchBy = 'city'
-        //             }
-        //             let breweries = [];
-        //             // google places key
-        //             const cors = "http://localhost:8080/"
-        //             const key = process.env.REACT_APP_GOOGLE_PLACES_KEY
-        //             const breweryDB = await axios.get(`https://api.openbrewerydb.org/breweries?by_${searchBy}=${ent}`)
-        //             // console.log(breweryDB)
-        //             for (var i = 0; i <= breweryDB.data.length - 1; i++) {
-        //                 if (breweryDB.data[i].name && breweryDB.data[i].name.length < 32 && breweryDB.data[i].street && breweryDB.data[i].city && breweryDB.data[i].state && breweryDB.data[i].website_url) {
-        //                     // implement google places API here  
-        //                     const google = await axios.get(`${cors}https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${breweryDB.data[i].name}%20${breweryDB.data[i].city}&inputtype=textquery&fields=photos&key=${key}`)
-        //                     breweries.push(
-        //                         {
-        //                         name: breweryDB.data[i].name,
-        //                         street: breweryDB.data[i].street,
-        //                         city: breweryDB.data[i].city,
-        //                         state: breweryDB.data[i].state,
-        //                         website_url: breweryDB.data[i].website_url,
-        //                         mapsQuery: `https://www.google.com/maps/search/?api=1&query=${breweryDB.data[i].name}%20${breweryDB.data[i].city}`,
-        //                         googleImage: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${google.data.candidates[0].photos[0].photo_reference}&key=${key}`
-        //                     })
-        //                 } 
-        //             }
-        //             says = {
-        //                 speaks: 'bot',
-        //                 msg: msg,
-        //                 entity: ent,
-        //                 action: action,
-        //                 breweries: breweries
-        //             }
-
-        //         // Trigger get-beer action
-        //         } else if (action === 'get-beer') {
-        //             let ent = anyEntity.stringValue;
-        //             const utID = process.env.REACT_APP_UNTAPPD_ID;
-        //             const utSecret = process.env.REACT_APP_UNTAPPD_SECRET;
-        //             const untappdURL = `https://api.untappd.com/v4/search/beer?q=${ent}&client_id=${utID}&client_secret=${utSecret}` ;
-        //             let beers = [];
-
-        //             const beerDB = await axios.get(untappdURL);
-        //             console.log(beerDB)
-        //             for (var i = 0;  i <= 4; i++) {
-        //                 beers.push({
-        //                     name: beerDB.data.response.beers.items[i].beer.beer_name,
-        //                     description: beerDB.data.response.beers.items[i].beer.beer_description,
-        //                     brewery: beerDB.data.response.beers.items[i].brewery.brewery_name,
-        //                     beer_style: beerDB.data.response.beers.items[i].beer.beer_style,
-        //                     abv: beerDB.data.response.beers.items[i].beer.beer_abv,
-        //                     image: beerDB.data.response.beers.items[i].beer.beer_label,
-        //                     url: beerDB.data.response.beers.items[i].brewery.contact.url
-        //                 })
-        //             }
-        //             says = {
-        //                 speaks: 'bot',
-        //                 msg: msg,
-        //                 entity: ent,
-        //                 action: action,
-        //                 beers: beers
-        //             }
-        //         } else if (action === 'get-ride') {
-        //             let rides = [];
-
-        //             rides.push({
-        //                 title: 'Uber',
-        //                 image: 'https://images.ctfassets.net/37l920h5or7f/5veFGObZjqmQY8qKu6auAW/abe271ddb25ae87d1212a4da798d3229/asset-030.jpg?fm=jpg&q=70&w=1600',
-        //                 link: 'https://auth.uber.com/login/?breeze_local_zone=phx3&next_url=https%3A%2F%2Fm.uber.com%2Flooking%3F_ga%3D2.243517442.640375780.1610728947-973137274.1609435436&state=Z16bRV1Gw6C1gEXZANKkwBg-antk71SbGUeGmAKfO6w%3D',
-        //             })
-
-        //             rides.push({
-        //                 title: 'Lyft',
-        //                 image: 'https://lh3.googleusercontent.com/pw/ACtC-3dFlWRhFhZzgLNnPvX9k0cq3GcVHz57FdpCEZrXV4lMvHTFl0hYtMwZsfyS1m5O-SxIZf3rssT2YfqEjYCwAv3ggWtP5stt2XNuXjQyo_ym3Y1niD9cUCPEG8aZzJbMkJDQwJXT0iQXKEsvgBqALhmM=w1136-h639-no?authuser=0',
-        //                 link: 'https://ride.lyft.com/',
-        //             })
-
-        //             says = {
-        //                 speaks: 'bot',
-        //                 msg: msg,
-        //                 action: action,
-        //                 rides: rides
-        //             }
-        //         } else {
-        //             says = {
-        //                 speaks: 'bot',
-        //                 msg: msg
-        //             }
-        //         }
-        //         this.setState({messages: [...this.state.messages, says]});
-        //     }
-        // } catch (e) {
-        //     let says = {
-        //         speaks: 'bot',
-        //         msg: {
-        //             text: {
-        //                 text: "I'm sorry but it looks like one of my servers had a few too many. I need to shut down momentarily. Try again later."
-        //             }
-        //         } 
-        //     };
-        //     this.setState({messages: [...this.state.messages, says]});
-        // }
         const request = {
             queryInput: {
                 text: {
@@ -216,16 +98,11 @@ class Chatbot extends Component {
             let anyEntity = res.data.queryResult.parameters.any;
             let cityEntity = res.data.queryResult.parameters["geo-city"];
 
-            if (action === 'get-beer') {
-                console.log('beer')
-                console.log(anyEntity)
-            }
-
             if (res.data.queryResult.fulfillmentMessages) {
                 for (let msg of res.data.queryResult.fulfillmentMessages) {
                     // Change JSON response structure dependent on action
                            if ( action === 'get-name'  || action === 'get-city' ) {
-                        console.log(`Action: ${action}`)
+                        // console.log(`Action: ${action}`)
                         let ent = ''
                         let searchBy = ''
                         if (anyEntity !== undefined) {
@@ -243,7 +120,7 @@ class Chatbot extends Component {
                             var cors = "http://localhost:8080/"
                         }
                         
-                        console.log(cors)
+                        // console.log(cors)
                         const key = process.env.REACT_APP_GOOGLE_PLACES_KEY
                         const breweryDB = await axios.get(`https://api.openbrewerydb.org/breweries?by_${searchBy}=${ent}`)
                         // console.log(breweryDB)
@@ -280,7 +157,7 @@ class Chatbot extends Component {
                         let beers = [];
 
                         const beerDB = await axios.get(untappdURL);
-                        console.log(beerDB)
+                        // console.log(beerDB)
                         for (var i = 0;  i <= 4; i++) {
                             beers.push({
                                 name: beerDB.data.response.beers.items[i].beer.beer_name,
